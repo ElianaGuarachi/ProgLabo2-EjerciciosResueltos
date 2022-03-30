b6fa94bb1ad9164f8595d5d166d9c680a0a37b6d
ElianaGuarachi <eliana.guarachi23@gmail.com>
2022-03-29 21:57:08 -0300

    Vamos de a poco

---
+++ 04 - Sobrecargas/EjercicioI02-Cotizador/EjercicioI02-Cotizador/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Billetes;
+
+namespace EjercicioI02_Cotizador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

b7465590a55d9b0651648deb9eb3509c4361dd17
ElianaGuarachi <eliana.guarachi23@gmail.com>
2022-03-29 22:04:30 -0300

    de a poco

---
+++ 01 - Introduccion/EjercicioI04/EjercicioI04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EjercicioI04
+{
+    /*
+     * Consigna#
+        Un número perfecto es un entero positivo, que es igual a la suma de todos los enteros positivos 
+        (excluido el mismo) que son divisores del número.
+        El primer número perfecto es 6, ya que los divisores de 6 son 1, 2 y 3; y 1 + 2 + 3 = 6.
+        Escribir una aplicación que encuentre los 4 primeros números perfectos.
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
